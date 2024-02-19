6a855f268d151c91d789cd4e4d03eda2edad69f6
Stu Eggerton <gitstua@users.noreply.github.com>
2024-02-19 16:44:51 +1100

    fix purpose text

--- set-runner-group.sh
+++ set-runner-group.sh
@@ -2,7 +2,7 @@
 
 echo "------------------------------------------------------------"
 echo SCRIPT: $0
-echo PURPOSE: Retrieves the count of commits for each repository in a given organization
+echo PURPOSE: Demo of updating a runner group from a gh cli shell script
 echo PRE-REQUISITES: see https://github.com/gitstua/stu-scripts#pre-requisites
 echo DISCLAIMER: NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 echo "------------------------------------------------------------"
